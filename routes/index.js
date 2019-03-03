f01b8baa16ad8867f5c912a48dbfaff427a51667
Lucas JAHIER <lucas@lucasjahier.fr>
2019-03-03 12:57:25 +0100

    update

--- routes/index.js
+++ routes/index.js
@@ -1,70 +1,68 @@
 module.exports = (app, ejs, fs, mysql, crypto) => {
-    let connection;
+    // create the connection to database
+    const connection = mysql.createConnection({
+        host: 'bjowbrtr2vxn5ayw1toe-mysql.services.clever-cloud.com',
+        user: 'ucsp60dhtoagqt4d',
+        database: 'bjowbrtr2vxn5ayw1toe',
+        password: 'kFmmZjv7OGHc3AJUVwa9'
+    });
 
-    try {
-        let config = require('./config.json');
-        pool  = mysql.createPool({
-            connectionLimit : 5,
-            host            : config.database.host,
-            user            : config.database.user,
-            password        : config.database.password,
-            database        : config.database.database
-        });
-    } catch (e) {
-        pool  = mysql.createPool({
-            connectionLimit : 5,
-            host            : process.env.MYSQL_ADDON_HOST,
-            user            : process.env.MYSQL_ADDON_USER,
-            password        : process.env.MYSQL_ADDON_PASSWORD,
-            database        : process.env.MYSQL_ADDON_DB
-        });
-    }
-
-    app.get('/', (req, res) => {
-        fs.readFile('data.txt', (err, data) => {
-            if (err) throw err;
-
-            res.render('pages/index', {
-                data: data
-            })
-        });
-    })
-
-    // Login
-    app.get('/login', (req, res) => {
-        res.render('pages/login/index')
-    })
-
-    app.post('/login', (req, res) => {
-        let formPassword = crypto.createHmac('sha256', req.body.password);
-        pool.query('SELECT * FROM users WHERE password = ?', formPassword, function (error, results, fields) {
-            if (error) throw error;
-            console.log(results);
-        });
-    })
-
-    app.get('/water/:data', (req, res) => {
-        res.send();
-        let sensorData = {sensor: "water", data: req.params.data}
-        // execute will internally call prepare and query
-        pool.execute(
-            'INSERT INTO data SET ?',
-            sensorData,
-            function(err, results, fields) {
-                console.log(err);
-                console.log(results); // results contains rows returned by server
-                console.log(fields); // fields contains extra meta data about results, if available
-            }
-        );
-    })
-
-    app.get('/temperature/:data', (req, res) => {
-        res.send(req.params.data);
-    })
-    app.get('/lightning/:data', (req, res) => {
-        fs.writeFile('data.txt', req.params.data, (err) => {
-            if (err) throw err;
-            res.send(req.params.data);
-        });
-    })
+    // simple query
+    connection.query(
+        'SELECT * FROM `data`',
+        function(err, results, fields) {
+            console.log(results); // results contains rows returned by server
+            console.log(fields); // fields contains extra meta data about results, if available
+        }
+    );
+    // let config = require('../config.json');
+    // let connection  = mysql.createConnection({
+    //     connectionLimit : 5,
+    //     host            : "bjowbrtr2vxn5ayw1toe-mysql.services.clever-cloud.com",
+    //     user            : "ucsp60dhtoagqt4d",
+    //     password        : "kFmmZjv7OGHc3AJUVwa9",
+    //     database        : "bjowbrtr2vxn5ayw1toe",
+    // });
+    //
+    // app.get('/', (req, res) => {
+    //     res.json({"data": "2222"})
+    // })
+    //
+    // app.get('/login', (req, res) => {
+    //     res.render('pages/login/index')
+    // })
+    //
+    // // app.post('/login', (req, res) => {
+    // //     let formPassword = crypto.createHmac('sha256', req.body.password);
+    // //     connection.query('SELECT * FROM users WHERE password = ?', formPassword, function (error, results, fields) {
+    // //         if (error) throw error;
+    // //         console.log(results);
+    // //     });
+    // // })
+    //
+    // app.get('/water/:data', (req, res) => {
+    //     res.send();
+    //     let sensorData = {sensor: "water", data: req.params.data}
+    //
+    //     connection.execute(
+    //         'INSERT INTO data SET ?',
+    //         sensorData,
+    //         function(err, results, fields) {
+    //             console.log(err);
+    //             console.log(results);
+    //             console.log(fields);
+    //         }
+    //     );
+    // })
+    //
+    // app.get('/temperature/:data', (req, res) => {
+    //     res.send(req.params.data);
+    // })
+    //
+    // app.get('/lightning/:data', (req, res) => {
+    //     fs.writeFile('data.txt', req.params.data, (err) => {
+    //         if (err) throw err;
+    //         res.send(req.params.data);
+    //     });
+    // })
 }
